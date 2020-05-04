15cd162fcc837c707dbafb20ceed9f3639c1c7b8
Nuno Amaral <nuno.amaral@academiadecodigo.org>
2020-05-04 18:38:56 +0100

    finished module-3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -7,7 +7,7 @@ const Tile = (props) => {
   const color = props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className='Tile' style={color}>
+    <div className='Tile' style={color} onClick={() => props.handleTileClicked(props.id, props.color)}>
       {props.selected || props.matched ? <props.svg /> : null}
     </div>
   )
